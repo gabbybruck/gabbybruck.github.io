f88de5e735d8f937055f5742944e71a57d7ac4eb
gabbybruck <gabby.bruck@gmail.com>
2021-12-31 22:17:53 -0500

    illustration page

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from '../shared/services/content.service';
 import { ActivatedRoute } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.css']
+  styleUrls: ['./page.component.css'],
+  animations: [trigger('fadeSlideInOut', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateY(10px)' }),
+      animate('1000ms', style({ opacity: 1, transform: 'translateY(0)' })),
+    ]),
+    transition(':leave', [
+      animate('1000ms', style({ opacity: 0, transform: 'translateY(10px)' })),
+    ]),
+  ]),]
 })
 export class PageComponent implements OnInit {
   page: any;
--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ContentService {
   pages: any = {
-    'home': {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', images:[]},
-    'illustration': {title: 'ILLUSTRATION', subtitle: 'Welcome Home!', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg']},
-    'ux/ui': {title: 'UX/UI', subtitle: 'Welcome Home!', content: 'Some home content.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg']},
-    'about': {title: 'About', subtitle: 'About Us', content: 'Some content about us.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg']},
-    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'How to contact us.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg']}
+    'home': {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', images:[],logo:'assets/gb-logo.png'},
+    'illustration': {title: 'ILLUSTRATION', subtitle: 'Welcome Home!', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg', 'assets/game.jpg','assets/pele.jpg','assets/tv-room.JPG', 'assets/cooking.jpg'], imagesRight:['assets/fruits.jpg','assets/snake.jpg', 'assets/phone.jpg', 'assets/mango.JPG','assets/red.jpg', 'assets/rihanna.jpg'],logo:'assets/gb-logo.png'},
+    'ux/ui': {title: 'UX/UI', subtitle: 'Welcome Home!', content: 'Some home content.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg'], logo:'assets/gb-logo.png'},
+    'about': {title: 'About', subtitle: 'About Us', content: 'Some content about us.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg'],logo:'assets/gb-logo.png'},
+    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'How to contact us.', imagesLeft:['assets/hands.jpg', 'assets/purple.jpg'], imagesRight:['assets/hands.jpg', 'assets/purple.jpg'],logo:'assets/gb-logo.png'}
   };
 
 }
